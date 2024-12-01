645c265b0607b7e47ee0eda45417b5bd6bb243fc
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-12-01 16:28:32 +0200

    add endpoints

--- backend-app/src/main/java/red/theatre/backendapp/controller/ScheduleController.java
+++ backend-app/src/main/java/red/theatre/backendapp/controller/ScheduleController.java
@@ -1,11 +1,27 @@
 package red.theatre.backendapp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import red.theatre.backendapp.dto.performance.FullPerformanceResponseDTO;
+import red.theatre.backendapp.dto.performance.SimplePerformanceResponseDTO;
+import red.theatre.backendapp.service.Schedule;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/schedule")
 @RequiredArgsConstructor
 public class ScheduleController {
+    private final Schedule schedule;
+    @GetMapping
+    public List<SimplePerformanceResponseDTO> getAllPerformances() {
+        return schedule.getAllPerformances();
+    }
+    @GetMapping("/{id}")
+    public FullPerformanceResponseDTO getPerformanceById(@PathVariable("id") Long id) {
+        return schedule.getPerformanceById(id);
+    }
 }
